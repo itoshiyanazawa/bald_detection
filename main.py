918ffba207814a46f0e5f7198ad44bcf872dbf3c
mikio303 <yanazawaitoshi@yanagisawaaishinoMacBook-Pro.local>
2021-11-28 21:44:20 +0900

    1st commit

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import streamlit as st
 from PIL import Image
-
+from PIL import ImageFont
 import requests
 from PIL import ImageDraw
 import io
 
-st.title('顔認識アプリ')
+st.title('ハゲ度Check!')
+
+st.subheader('ハゲ度の目安')
+st.write('0~25%:　:smile:, 25~30%: :worried:, 31~100%: :innocent:')
+
 
 KEY = "5f47d596a70445c599391658743da07d"
 assert KEY
@@ -13,7 +17,7 @@ assert KEY
 face_api_url = 'https://20211128.cognitiveservices.azure.com/' + '/face/v1.0/detect'
 
 
-uploaded_file = st.file_uploader('Choose an image...', type='jpg')
+uploaded_file = st.file_uploader('JPG形式の画像を選択してください', type='jpg')
 if uploaded_file is not None:
     img = Image.open(uploaded_file)
 
@@ -27,7 +31,7 @@ if uploaded_file is not None:
         }
 
     params = {
-        'returnFaceAttributes': 'age,gender,smile,facialHair,headPose,glasses',
+        'returnFaceAttributes': 'age,gender,smile,facialHair,headPose,glasses,Hair',
         'returnFaceId': 'true'
     }
 
@@ -38,11 +42,28 @@ if uploaded_file is not None:
     results = response.json()
 
     for result in results:
+        bald = result['faceAttributes']['hair']['bald']
         result = result['faceRectangle']
+
+        if bald >= 0.31:
+            st.write('Result: :innocent:')
+        
+        elif bald >= 0.25:
+            st.write('Result: :worried:') 
+
+        else:
+            st.write('Result: :smile:')
+        
+       
 
         draw = ImageDraw.Draw(img)
         draw.rectangle([(result['left'],result['top']),(result['left']+result['width'],result['top']+result['height'])],
         fill=None, outline='green',width=5)
+        font = ImageFont.truetype("40231058178.ttf",50)
+        draw.text((result['left'],result['top']-60),'ハゲ度: {}%'.format(bald),(255,0,255),font=font)
 
     st.image(img, caption='Uploaded Image.', use_column_width=True)
+    st.write('※判断基準はあくまで目安です。誤認識する場合があります。')
+
+
 
